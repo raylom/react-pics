0ee77eb27ece98141fbae8736b1bf65ff2c2b879
raylom <rlom96@gmail.com>
2019-05-06 16:21:16 -0500

    refactored onsearchSubmit to use async, await, and be an arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
-  async onSearchSubmit(term) {
+
+  onSearchSubmit = async term => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
@@ -13,12 +14,14 @@ class App extends React.Component {
           "Client-ID 1c741f89166a3295ddc690c308c9957ad668beb66643625579c3785f77ce38d6 "
       }
     });
-    console.log(response.data.results);
-  }
+    console.log(this);
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found {this.state.images.length} images
       </div>
     );
   }
