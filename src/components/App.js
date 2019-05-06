86fde9528d001672101df4c686cdebf9a4affdc7
raylom <rlom96@gmail.com>
2019-05-06 16:03:53 -0500

    added state object to class App

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,16 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] };
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization:
           "Client-ID 1c741f89166a3295ddc690c308c9957ad668beb66643625579c3785f77ce38d6 "
       }
     });
+    console.log(response.data.results);
   }
   render() {
     return (
